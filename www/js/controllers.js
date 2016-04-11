bfd8e09b55cbce7b22d26386d1dd5aa3143c875c
Lucas Caetano <lucasclopesr@gmail.com>
2016-04-11 20:06:16 -0300

    toggle screen

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,5 +1,9 @@
 angular.module('starter.controllers', [])
 
 .controller('AppCtrl', function($scope, $ionicModal, $timeout) {
-	//-- Content here
+	$scope.settingsList = [
+	    { text: "Wireless", checked: true },
+	    { text: "GPS", checked: false },
+	    { text: "Bluetooth", checked: false }
+	  ];
 });
