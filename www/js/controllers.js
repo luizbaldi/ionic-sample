621e37622adf6dbc88a6ee59be3ed353ca05292b
Lucas Caetano <lucasclopesr@gmail.com>
2016-04-13 22:44:40 -0300

    loading items list using controller

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,8 +2,14 @@ angular.module('starter.controllers', [])
 
 .controller('AppCtrl', function($scope, $ionicModal, $timeout) {
 	$scope.settingsList = [
-	    { text: "Wireless", checked: true },
-	    { text: "GPS", checked: false },
-	    { text: "Bluetooth", checked: false }
-	  ];
+    		{ text: "Wireless", checked: true },
+    		{ text: "GPS", checked: false },
+    		{ text: "Bluetooth", checked: false }
+	 ];
+
+	 $scope.listItems = [
+		{name: "Item 1"},
+	 	{name: "Item 2"},
+	 	{name: "Item 3"}
+	 ];
 });
