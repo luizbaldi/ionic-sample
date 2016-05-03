9219a99fad497d61c9a90ef0b8075f02f664d51d
Luiz Baldi <baldilp@gmail.com>
2016-05-02 22:00:15 -0300

    basic components finished

--- www/js/app.js
+++ www/js/app.js
@@ -6,306 +6,306 @@
 angular.module('starter', ['ionic', 'starter.controllers'])
 
 .run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-      // for form inputs)
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+	$ionicPlatform.ready(function() {
+		if (window.cordova && window.cordova.plugins.Keyboard) {
+			// Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+			// for form inputs)
+			cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
 
-      // Don't remove this line unless you know what you are doing. It stops the viewport
-      // from snapping when text inputs are focused. Ionic handles this internally for
-      // a much nicer keyboard experience.
-      cordova.plugins.Keyboard.disableScroll(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
+			// Don't remove this line unless you know what you are doing. It stops the viewport
+			// from snapping when text inputs are focused. Ionic handles this internally for
+			// a much nicer keyboard experience.
+			cordova.plugins.Keyboard.disableScroll(true);
+		}
+		if (window.StatusBar) {
+			StatusBar.styleDefault();
+		}
+	});
 })
 
 .config(function($stateProvider, $urlRouterProvider) {
-  $stateProvider
+	$stateProvider
 
-  .state('app', {
-    url: '/app',
-    abstract: true,
-    templateUrl: 'templates/menu.html',
-    controller: 'MenuCtrl'
-  })
-  
-  .state('app.home', {
-    url: '/home',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/home.html'
-      }
-    }
-  })
+	   .state('app', {
+		url: '/app',
+		abstract: true,
+		templateUrl: 'templates/menu.html',
+		controller: 'MenuCtrl'
+	})
 
-  //-- Buttons Menu
-  .state('app.buttonsBasic', {
-    url: '/buttonsBasic',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/buttons/basic.html'
-      }
-    }
-  })
+	.state('app.home', {
+		url: '/home',
+		views: {
+			'menuContent': {
+				templateUrl: 'templates/home.html'
+			}
+		}
+	})
 
-  .state('app.buttonsBlock', {
-    url: '/buttonsBlock',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/buttons/block.html'
-      }
-    }
-  })
+	//-- Buttons Menu
+	.state('app.buttonsBasic', {
+		url: '/buttonsBasic',
+		views: {
+			'menuContent': {
+				templateUrl: 'templates/buttons/basic.html'
+			}
+		}
+	})
 
-  .state('app.buttonsIcons', {
-    url: '/buttonsIcons',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/buttons/icons.html'
-      }
-    }
-  })
+	.state('app.buttonsBlock', {
+		url: '/buttonsBlock',
+		views: {
+			'menuContent': {
+				templateUrl: 'templates/buttons/block.html'
+			}
+		}
+	})
 
-  .state('app.buttonsFullWidth', {
-    url: '/buttonsFullWidth',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/buttons/full-width.html'
-      }
-    }
-  })
+	.state('app.buttonsIcons', {
+		url: '/buttonsIcons',
+		views: {
+			'menuContent': {
+				templateUrl: 'templates/buttons/icons.html'
+			}
+		}
+	})
 
-  .state('app.buttonsOutlined', {
-    url: '/buttonsOutlined',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/buttons/outlined.html'
-      }
-    }
-  })
+	.state('app.buttonsFullWidth', {
+		url: '/buttonsFullWidth',
+		views: {
+			'menuContent': {
+				templateUrl: 'templates/buttons/full-width.html'
+			}
+		}
+	})
 
-  .state('app.buttonsClear', {
-    url: '/buttonsClear',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/buttons/clear.html'
-      }
-    }
-  })
+	.state('app.buttonsOutlined', {
+		url: '/buttonsOutlined',
+		views: {
+			'menuContent': {
+				templateUrl: 'templates/buttons/outlined.html'
+			}
+		}
+	})
 
-  .state('app.buttonsBar', {
-    url: '/buttonsBar',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/buttons/bar.html'
-      }
-    }
-  })
+	.state('app.buttonsClear', {
+		url: '/buttonsClear',
+		views: {
+			'menuContent': {
+				templateUrl: 'templates/buttons/clear.html'
+			}
+		}
+	})
 
-  //-- List Menu
-  .state('app.listBasic', {
-    url: '/listBasic',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/list/basic.html'
-      }
-    }
-  })
+	.state('app.buttonsBar', {
+		url: '/buttonsBar',
+		views: {
+			'menuContent': {
+				templateUrl: 'templates/buttons/bar.html'
+			}
+		}
+	})
 
-  .state('app.listAvatars', {
-    url: '/listAvatars',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/list/avatars.html'
-      }
-    }
-  })
+	//-- List Menu
+	.state('app.listBasic', {
+		url: '/listBasic',
+		views: {
+			'menuContent': {
+				templateUrl: 'templates/list/basic.html'
+			}
+		}
+	})
 
-  .state('app.listButtons', {
-    url: '/listButtons',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/list/buttons.html'
-      }
-    }
-  })
+	.state('app.listAvatars', {
+		url: '/listAvatars',
+		views: {
+			'menuContent': {
+				templateUrl: 'templates/list/avatars.html'
+			}
+		}
+	})
 
-  .state('app.listDivider', {
-    url: '/listDivider',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/list/divider.html'
-      }
-    }
-  })
+	.state('app.listButtons', {
+		url: '/listButtons',
+		views: {
+			'menuContent': {
+				templateUrl: 'templates/list/buttons.html'
+			}
+		}
+	})
 
-  .state('app.listIcons', {
-    url: '/listIcons',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/list/icons.html'
-      }
-    }
-  })
+	.state('app.listDivider', {
+		url: '/listDivider',
+		views: {
+			'menuContent': {
+				templateUrl: 'templates/list/divider.html'
+			}
+		}
+	})
 
-  .state('app.listInset', {
-    url: '/listInset',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/list/inset.html'
-      }
-    }
-  })
+	.state('app.listIcons', {
+		url: '/listIcons',
+		views: {
+			'menuContent': {
+				templateUrl: 'templates/list/icons.html'
+			}
+		}
+	})
 
-  .state('app.listThumbnails', {
-    url: '/listThumbnails',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/list/thumbnails.html'
-      }
-    }
-  })
+	.state('app.listInset', {
+		url: '/listInset',
+		views: {
+			'menuContent': {
+				templateUrl: 'templates/list/inset.html'
+			}
+		}
+	})
 
-  .state('app.form', {
-    url: '/form',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/form/form.html'
-      }
-    }
-  })
+	.state('app.listThumbnails', {
+		url: '/listThumbnails',
+		views: {
+			'menuContent': {
+				templateUrl: 'templates/list/thumbnails.html'
+			}
+		}
+	})
 
-  .state('app.toggle', {
-    url: '/toggle',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/toggle/toggle.html'
-      }
-    }
-  })
+	.state('app.form', {
+		url: '/form',
+		views: {
+			'menuContent': {
+				templateUrl: 'templates/form/form.html'
+			}
+		}
+	})
 
-  .state('app.checkbox', {
-    url: '/checkbox',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/checkbox/checkbox.html'
-      }
-    }
-  })
+	.state('app.toggle', {
+		url: '/toggle',
+		views: {
+			'menuContent': {
+				templateUrl: 'templates/toggle/toggle.html'
+			}
+		}
+	})
 
-  .state('app.radioButton', {
-    url: '/radioButton',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/radio-buttom/radio-buttom.html'
-      }
-    }
-  })
+	.state('app.checkbox', {
+		url: '/checkbox',
+		views: {
+			'menuContent': {
+				templateUrl: 'templates/checkbox/checkbox.html'
+			}
+		}
+	})
 
-  .state('app.range', {
-    url: '/range',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/range/range.html'
-      }
-    }
-  })
+	.state('app.radioButton', {
+		url: '/radioButton',
+		views: {
+			'menuContent': {
+				templateUrl: 'templates/radio-buttom/radio-buttom.html'
+			}
+		}
+	})
 
-  .state('app.select', {
-    url: '/select',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/select/select.html'
-      }
-    }
-  })
+	.state('app.range', {
+		url: '/range',
+		views: {
+			'menuContent': {
+				templateUrl: 'templates/range/range.html'
+			}
+		}
+	})
 
-  //-- Tabs Menu
-  .state('app.tabsIconOnly', {
-    url: '/tabsIconOnly',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/tabs/icon-only.html'
-      }
-    }
-  })
+	.state('app.select', {
+		url: '/select',
+		views: {
+			'menuContent': {
+				templateUrl: 'templates/select/select.html'
+			}
+		}
+	})
 
-  .state('app.tabsLeftIcon', {
-    url: '/tabsLeftIcon',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/tabs/left-icon.html'
-      }
-    }
-  })
+	//-- Tabs Menu
+	.state('app.tabsIconOnly', {
+		url: '/tabsIconOnly',
+		views: {
+			'menuContent': {
+				templateUrl: 'templates/tabs/icon-only.html'
+			}
+		}
+	})
 
-  .state('app.tabsStripedIcon', {
-    url: '/tabsStripedIcon',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/tabs/striped-icon.html'
-      }
-    }
-  })
+	.state('app.tabsLeftIcon', {
+		url: '/tabsLeftIcon',
+		views: {
+			'menuContent': {
+				templateUrl: 'templates/tabs/left-icon.html'
+			}
+		}
+	})
 
-  .state('app.tabsTopIcon', {
-    url: '/tabsTopIcon',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/tabs/top-icon.html'
-      }
-    }
-  })
+	.state('app.tabsStripedIcon', {
+		url: '/tabsStripedIcon',
+		views: {
+			'menuContent': {
+				templateUrl: 'templates/tabs/striped-style.html'
+			}
+		}
+	})
 
-  //-- Grid Menu
-  .state('app.gridEvenColumns', {
-    url: '/gridEvenColumns',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/grid/even-columns.html'
-      }
-    }
-  })
+	.state('app.tabsTopIcon', {
+		url: '/tabsTopIcon',
+		views: {
+			'menuContent': {
+				templateUrl: 'templates/tabs/top-icon.html'
+			}
+		}
+	})
 
-  .state('app.gridExplicitColumn', {
-    url: '/gridExplicitColumn',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/grid/explicit-column-sizes.html'
-      }
-    }
-  })
+	//-- Grid Menu
+	.state('app.gridEvenColumns', {
+		url: '/gridEvenColumns',
+		views: {
+			'menuContent': {
+				templateUrl: 'templates/grid/even-columns.html'
+			}
+		}
+	})
 
-  .state('app.gridOffsetColumn', {
-    url: '/gridOffsetColumn',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/grid/explicit-column-sizes.html'
-      }
-    }
-  })
+	.state('app.gridExplicitColumn', {
+		url: '/gridExplicitColumn',
+		views: {
+			'menuContent': {
+				templateUrl: 'templates/grid/explicit-column-sizes.html'
+			}
+		}
+	})
 
-  .state('app.gridResponsive', {
-    url: '/gridResponsive',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/grid/responsive.html'
-      }
-    }
-  })
+	.state('app.gridOffsetColumn', {
+		url: '/gridOffsetColumn',
+		views: {
+			'menuContent': {
+				templateUrl: 'templates/grid/explicit-column-sizes.html'
+			}
+		}
+	})
 
-  .state('app.gridVerticallyAlligned', {
-    url: '/gridVerticallyAlligned',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/grid/vertically-aligned.html'
-      }
-    }
-  });
+	.state('app.gridResponsive', {
+		url: '/gridResponsive',
+		views: {
+			'menuContent': {
+				templateUrl: 'templates/grid/responsive.html'
+			}
+		}
+	})
 
-  $urlRouterProvider.otherwise('/app/home');
+	.state('app.gridVerticallyAlligned', {
+		url: '/gridVerticallyAlligned',
+		views: {
+			'menuContent': {
+				templateUrl: 'templates/grid/vertically-aligned.html'
+			}
+		}
+	});
+
+	$urlRouterProvider.otherwise('/app/home');
 });
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,28 +2,43 @@ angular.module('starter.controllers', [])
 
 .controller('MenuCtrl', function($scope, $ionicModal, $timeout) {
 	$scope.openSub = function(name) {
-	    $scope.submenu = true;
-	    $scope.selection = name;
+		$scope.submenu = true;
+		$scope.selection = name;
 	};
-	
+
 	$scope.backToMain = function() {
-	    $scope.submenu = false;
-	    $scope.selection = 'main';
+		$scope.submenu = false;
+		$scope.selection = 'main';
 	};
 
 	$scope.settingsList = [
-		{ text: "Wireless", checked: true },
-		{ text: "GPS", checked: false },
-		{ text: "Bluetooth", checked: false }
+		{
+			text: "Wireless",
+			checked: true
+		},
+		{
+			text: "GPS",
+			checked: false
+		},
+		{
+			text: "Bluetooth",
+			checked: false
+		}
 	];
 
 	/* toDo
-	Create a separate file for List Controller 
+	Create a separate file for List Controller
 	-- */
 	$scope.listItems = [
-		{name: "Item 1"},
-		{name: "Item 2"},
-	 	{name: "Item 3"}
+		{
+			name: "Item 1"
+		},
+		{
+			name: "Item 2"
+		},
+		{
+			name: "Item 3"
+		}
 	];
 
 	$scope.listContacts = [
@@ -40,4 +55,30 @@ angular.module('starter.controllers', [])
 			description: "I'm a Banana!"
 		}
 	]
+
+	$scope.show = function() {
+
+   		// Show the action sheet
+		var hideSheet = $ionicActionSheet.show({
+			buttons: [{
+				text: '<b>Share</b> This'
+			}, {
+				text: 'Move'
+			}],
+			destructiveText: 'Delete',
+			titleText: 'Modify your album',
+			cancelText: 'Cancel',
+			cancel: function() {
+				// add cancel code..
+			},
+			buttonClicked: function(index) {
+				return true;
+			}
+		});
+
+		// For example's sake, hide the sheet after two seconds
+		$timeout(function() {
+			hideSheet();
+		}, 2000);
+	};
 });
